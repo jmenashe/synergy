16cb68f67856300f5019429629d1d3914d8d8e93
Jacob Menashe <jmenashe@cs.utexas.edu>
2015-10-08 20:11:16 -0500

    Copying synergy server/client files on build.

--- hm.cmd
+++ hm.cmd
@@ -3,5 +3,7 @@
 python hm.py %*
 
 copy bin\Release\synergy.exe "C:\Program Files (x86)\Synergy\synergy.exe"
+copy bin\Release\synergyc.exe "C:\Program Files (x86)\Synergy\synergyc.exe"
+copy bin\Release\synergys.exe "C:\Program Files (x86)\Synergy\synergys.exe"
 
 exit /b %errorlevel%
